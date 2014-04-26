74e3381472375eb0ed9924a61c7f5d1a71bedc80
Hack Reactor Students <robertoatc02@gmail.com>
2014-04-25 17:16:14 -0700

    Add movement

--- watchout.js
+++ watchout.js
@@ -4,32 +4,68 @@ var svg = d3.select('body').append('svg')
   .attr('class', 'gameboard')
   .append('g');
 
+var boardWidth = 800;
+var boardHeight = 800;
+
+//debugger;
 var Circle = function() {
   var instance = Object.create(Circle.prototype);
-  instance.cx = 400;
-  instance.cy = 400;
+  instance.radius = 10;
+  instance.diameter = 20;
+  instance.cx = Math.random() * ((boardWidth - instance.diameter) - instance.diameter) + instance.diameter;
+  instance.cy = Math.random() * ((boardHeight - instance.diameter) - instance.diameter) + instance.diameter;
   instance.color = 'red';
   instance.direction = 0;
   return instance;
 };
 
-Circle.prototype.getCx = function() {
-  return this.cx;
+Circle.prototype.randomMove = function(){
+  // helper function that tells us which wall we're gonna hit
+  // depending on the direction and the current position
+
+  this.cx = Math.random() * ((boardWidth - this.diameter) - this.diameter) + this.diameter;
+  this.cy = Math.random() * ((boardWidth - this.diameter) - this.diameter) + this.diameter;
 };
+
 
 // Create circles
 // Put them inside array for d3 to work on
 
+var data = [];
 
+for (var i = 0; i < 15; i++){
+  data.push(Circle());
+}
 
-var data = [Circle()];
-
-var circle = svg.selectAll('circle').data(data);
+var circle = svg.selectAll('circle')
+                .data(data);
 
 circle.enter().append('circle');
 
 circle.style('fill', function(node) { return node.color; })
-      .attr('r', 10)
+      .attr('r', function(d) { return d.radius; })
       .attr('cx', function(d) { return d.cx; })
       .attr('cy', function(d) { return d.cy; });
+
+
+var update = function(data){
+// Get all circles from svg and append new data
+//debugger;
+  var circles = svg.selectAll('circle')
+    .data(data);
+
+  circles
+  .transition().duration(1000)
+  .attr('cx', function(d){
+    d.randomMove();
+    console.log('test');
+    return d.cx;
+  })
+  .attr('cy', function(d){ return d.cy; });
+};
+
+setInterval(function() {
+  update(data);
+}, 1000);
+
 
